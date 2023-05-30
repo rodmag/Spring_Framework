7f3420136c94cca6d8c68cbb3a3f827e375a27ca
rodmag <rodmag@outlook.com>
2023-05-30 15:48:21 -0300

    Initial Commit.

---
+++ twclientes/src/main/java/br/com/treinaweb/twclientes/TwclientesApplication.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.twclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwclientesApplication.class, args);
+		System.out.println("---> Server Running - Access with localhost <----");
+	}
+
+}
